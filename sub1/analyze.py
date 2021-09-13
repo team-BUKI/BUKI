d84433870ae3d42d793301d68019c96ccc6cdeae
박선아 <bsa8854@naver.com>
2021-09-13 10:54:11 +0900

    feature: all file removed
    Former-commit-id: 9e7832e6d9aea9da72055e2c30cfed30fcb3a6d3

---
+++ sub1/analyze.py
@@ -0,0 +1,71 @@
+# -*- coding: cp949 -*-
+from parse import load_dataframes
+import pandas as pd
+import shutil
+
+
+def sort_stores_by_score(dataframes, n=20, min_reviews=30):
+    """
+    Req. 1-2-1 각 음식점의 평균 평점을 계산하여 높은 평점의 음식점 순으로 `n`개의 음식점을 정렬하여 리턴합니다
+    Req. 1-2-2 리뷰 개수가 `min_reviews` 미만인 음식점은 제외합니다.
+    """
+    stores_reviews = pd.merge(
+        dataframes["stores"], dataframes["reviews"], left_on="id", right_on="store"
+    )
+ 
+    scores_group = stores_reviews.groupby(["store", "store_name"])
+    #pd.set_option('display.max_columns', None)
+    #print(scores_group.head())
+    temp = scores_group.filter(lambda g: len(g) > min_reviews).groupby(["store", "store_name"])
+    #print(temp)
+
+    scores = temp.mean() # 숫자들의 평균 값
+#    print(scores.head())
+
+    df_sorted_by_values = scores.sort_values(by="score", ascending=False) #평균평점으로 정렬
+ #   print(df_sorted_by_values.head())
+    
+    return df_sorted_by_values.head(n=n).reset_index()
+
+
+def get_most_reviewed_stores(dataframes, n=20):
+    """
+    Req. 1-2-3 가장 많은 리뷰를 받은 `n`개의 음식점을 정렬하여 리턴합니다
+    
+    """
+    stores_reviews = pd.merge(
+        dataframes["stores"], dataframes["reviews"], left_on="id", right_on="store"
+    )
+
+    
+    raise NotImplementedError
+
+
+def get_most_active_users(dataframes, n=20):
+    """
+    Req. 1-2-4 가장 많은 리뷰를 작성한 `n`명의 유저를 정렬하여 리턴합니다.
+    """
+    raise NotImplementedError
+
+
+def main():
+    data = load_dataframes()
+
+    term_w = shutil.get_terminal_size()[0] - 1
+    separater = "-" * term_w
+
+    stores_most_scored = sort_stores_by_score(data)
+
+    print("[최고 평점 음식점]")
+    print(f"{separater}\n")
+    for i, store in stores_most_scored.iterrows():
+        print(
+            "{rank}위: {store}({score}점)".format(
+                rank=i + 1, store=store.store_name, score=store.score
+            )
+        )
+    print(f"\n{separater}\n\n")
+
+
+if __name__ == "__main__":
+    main()
