44d435a8bdf34b51f49199e343d565a47b13fc2d
원지연 <yh3347@naver.com>
2021-09-14 16:11:09 +0900

    [#S05P21A303-22] feat: Add MyHeader Component
    Former-commit-id: b5dd4f230f68bce38d04b7b2e58e091f34ec88c8

---
+++ frontend/src/views/common/MyHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img src="@/assets/images/logo.svg" class="logo" />
+    <div class="search-icon">
+      <i class="fas fa-search"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./MyHeader.css";
+
+export default {
+  name: "Template",
+  components: {},
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {},
+};
+</script>
