3f16e00097eafa2aa767eee9e7118e2f2c75ff21
원지연 <yh3347@naver.com>
2021-09-14 16:12:43 +0900

    [#S05P21A303-16] feat: Add Mobile Template vue
    Former-commit-id: b4a72283c19d4c6b84e1015080da19db86345fdf

---
+++ frontend/src/views/template/Mobile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <my-header />
+    <div class="title title-3">타이틀</div>
+    <my-footer />
+  </div>
+</template>
+
+<script>
+import "./Mobile.css";
+import MyHeader from "@/views/common/MyHeader.vue";
+import MyFooter from "@/views/common/MyFooter.vue";
+
+export default {
+  name: "Template",
+  components: {
+    MyHeader,
+    MyFooter,
+  },
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {},
+};
+</script>
