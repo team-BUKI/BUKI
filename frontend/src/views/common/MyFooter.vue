63c9668e11431b55753ae339a55ad80f3cd07156
원지연 <yh3347@naver.com>
2021-09-14 16:12:00 +0900

    [#S05P21A303-23] feat: Add MyFooter Component
    Former-commit-id: dfb1048a77a19d24051a6e43b4ecff5b2fc5520f

---
+++ frontend/src/views/common/MyFooter.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="navbar">
+      <div class="menu">
+        <div class="menu-icon">
+          <i class="fas fa-home"></i>
+        </div>
+        <span class="menu-name title-7">홈</span>
+      </div>
+      <div class="menu">
+        <div class="menu-icon">
+          <i class="fas fa-bars"></i>
+        </div>
+        <span class="menu-name title-7">카테고리</span>
+      </div>
+      <div class="menu">
+        <div class="menu-icon">
+          <i class="fas fa-medal"></i>
+        </div>
+        <span class="menu-name title-7">랭킹</span>
+      </div>
+      <div class="menu">
+        <div class="menu-icon">
+          <i class="fas fa-edit"></i>
+        </div>
+        <span class="menu-name title-7">일기</span>
+      </div>
+      <div class="menu">
+        <div class="menu-icon">
+          <i class="fas fa-user"></i>
+        </div>
+        <span class="menu-name title-7">마이페이지</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./MyFooter.css";
+
+export default {
+  name: "Template",
+  components: {},
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {},
+};
+</script>
