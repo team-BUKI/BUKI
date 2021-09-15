32d58c799d8a8bcf79b00b694a98f96ef168e684
choieunsong <greatgelly96@gmail.com>
2021-09-15 16:58:31 +0900

    [#S05P21A303-30] feat: 포트바꿈
    Former-commit-id: 0fb291ba34d649adde82327612006226c6cdcf2e

--- frontend/src/views/mypage/MyPage.vue
+++ frontend/src/views/mypage/MyPage.vue
@@ -1,46 +1,63 @@
 <template>
-    <div class="container">
-        <div class="my-title title-3">마이페이지</div>
-        <img src="@/assets/images/myPage.svg" class="image" />
+  <div class="container">
+    <div class="my-title title-3">마이페이지</div>
+    <img src="@/assets/images/myPage.svg" class="image" />
 
-        <div class="login-container">
-            <div class="kakao-button">
-                <img src="@/assets/images/kakao.svg" class="kakao-icon" />
-                <div class="kakao-title title-4">
-                    <span>Kakao로 로그인</span>
-                </div>
-            </div>
-            <div class="google-button">
-                <img src="@/assets/images/google.svg" class="kakao-icon" />
-                <div class="kakao-title title-4">
-                    <span>Google로 로그인</span>
-                </div>
-            </div>
+    <div class="login-container">
+      <div class="kakao-button btn" @click="kakaoLogin">
+        <img src="@/assets/images/kakao.svg" class="kakao-icon" />
+        <div class="kakao-title title-4">
+          <span>Kakao로 로그인</span>
         </div>
-        <my-footer />
+      </div>
+      <div class="google-button btn" @click="googleLogin">
+        <img src="@/assets/images/google.svg" class="kakao-icon" />
+        <div class="kakao-title title-4">
+          <span>Google로 로그인</span>
+        </div>
+      </div>
     </div>
+    <my-footer :selected="'mypage'" />
+  </div>
 </template>
 
 <script>
 import MyFooter from "@/views/common/MyFooter.vue";
 
 export default {
-    name: "MyPage",
-    components: {
-        MyFooter,
+  name: "MyPage",
+  components: {
+    MyFooter,
+  },
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {
+    googleLogin() {
+      let token = localStorage.getItem("token");
+      if (token == "") {
+        //회원가입 페이지로 보내기
+      } else {
+        //로그인 페이지로 보내기
+      }
     },
-    // props
-    props: {},
-    // data
-    data() {
-        return {};
+    kakaoLogin() {
+      let token = localStorage.getItem("token");
+      if (token == "") {
+        //회원가입 페이지로 보내기
+      } else {
+        //로그인 페이지로 보내기
+      }
     },
-    // computed
-    computed: {},
-    // lifecycle hook
-    mounted() {},
-    // methods
-    methods: {},
+  },
 };
 </script>
 <style scoped src="./MyPage.css"></style>
