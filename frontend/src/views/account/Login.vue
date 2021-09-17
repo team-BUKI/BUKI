391ea4a14eed0eb310b96414cad7f8292b74edae
원지연 <yh3347@naver.com>
2021-09-17 12:02:05 +0900

    [#S05P21A303-26] chore: etc
    Former-commit-id: 1b9c7557947bb0d0cc0783fc09bdb736fa73fa38

--- frontend/src/views/account/Login.vue
+++ frontend/src/views/account/Login.vue
@@ -1,30 +1,24 @@
 <template>
-    <div>
-        <!-- 여기부터 코드 작성 -->
-    </div>
+  <div>
+    <!-- 여기부터 코드 작성 -->
+  </div>
 </template>
 
 <script>
-import MyHeader from "@/views/common/MyHeader.vue";
-import MyFooter from "@/views/common/MyFooter.vue";
-
 export default {
-    name: "Login",
-    components: {
-        MyHeader,
-        MyFooter,
-    },
-    // props
-    props: {},
-    // data
-    data() {
-        return {};
-    },
-    // computed
-    computed: {},
-    // lifecycle hook
-    mounted() {},
-    // methods
-    methods: {},
+  name: "Login",
+  components: {},
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {},
 };
 </script>
--- frontend/src/views/mypage/MyPage.vue
+++ frontend/src/views/mypage/MyPage.vue
@@ -1,46 +1,46 @@
 <template>
-    <div class="container">
-        <div class="my-title title-3">마이페이지</div>
-        <img src="@/assets/images/myPage.svg" class="image" />
+  <div class="container">
+    <div class="my-title title-3">마이페이지</div>
+    <img src="@/assets/images/myPage.svg" class="image" />
 
-        <div class="login-container">
-            <div class="kakao-button">
-                <img src="@/assets/images/kakao.svg" class="kakao-icon" />
-                <div class="kakao-title title-4">
-                    <span>Kakao로 로그인</span>
-                </div>
-            </div>
-            <div class="google-button">
-                <img src="@/assets/images/google.svg" class="kakao-icon" />
-                <div class="kakao-title title-4">
-                    <span>Google로 로그인</span>
-                </div>
-            </div>
+    <div class="login-container">
+      <div class="kakao-button">
+        <img src="@/assets/images/kakao.svg" class="kakao-icon" />
+        <div class="kakao-title title-4">
+          <span>Kakao로 로그인</span>
         </div>
-        <my-footer />
+      </div>
+      <div class="google-button">
+        <img src="@/assets/images/google.svg" class="kakao-icon" />
+        <div class="kakao-title title-4">
+          <span>Google로 로그인</span>
+        </div>
+      </div>
     </div>
+    <my-footer />
+  </div>
 </template>
 
 <script>
 import MyFooter from "@/views/common/MyFooter.vue";
 
 export default {
-    name: "MyPage",
-    components: {
-        MyFooter,
-    },
-    // props
-    props: {},
-    // data
-    data() {
-        return {};
-    },
-    // computed
-    computed: {},
-    // lifecycle hook
-    mounted() {},
-    // methods
-    methods: {},
+  name: "MyPage",
+  components: {
+    MyFooter,
+  },
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {},
 };
 </script>
 <style scoped src="./MyPage.css"></style>
