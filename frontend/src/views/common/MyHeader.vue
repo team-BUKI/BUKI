fb5cd7d508b9fd862c5329c6659514ae83a066fd
원지연 <yh3347@naver.com>
2021-09-17 11:52:14 +0900

    [#S05P21A303-22] chore: 기본 Header, Footer 수정
    Former-commit-id: 821935161e297941376ea3016a8b0d95830ba5c2

--- frontend/src/views/common/MyHeader.vue
+++ frontend/src/views/common/MyHeader.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="header-wrapper">
-        <div @click="$router.push({ name: 'Home' })">
-            <img src="@/assets/images/logo.svg" class="logo" />
-        </div>
-        <div class="icon-wrapper" @click="$router.push({ name: 'Search' })">
-            <i class="fas fa-search"></i>
-        </div>
+  <div class="header-wrapper">
+    <img
+      class="logo"
+      src="@/assets/images/logo.png"
+      @click="$router.push({ name: 'Home' })"
+    />
+    <div class="icon-wrapper" @click="$router.push({ name: 'Search' })">
+      <i class="fas fa-search"></i>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "MyHeader",
-    components: {},
-    // props
-    props: {},
-    // data
-    data() {
-        return {};
-    },
-    // computed
-    computed: {},
-    // lifecycle hook
-    mounted() {},
-    // methods
-    methods: {},
+  name: "MyHeader",
+  components: {},
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {},
 };
 </script>
 
