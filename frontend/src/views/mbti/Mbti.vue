4d54fa62fc146cf9c84b90b9033a5685b2538241
원지연 <yh3347@naver.com>
2021-09-17 11:53:26 +0900

    [#S05P21A303-106] design: MBTI 테스트 페이지 기본 틀 추가
    Former-commit-id: 5bfc58105e3c7010db3ed247b676d63511fd5048

---
+++ frontend/src/views/mbti/Mbti.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="container">
+      <my-header />
+      <div class="contents">
+        <span class="title title-3">MBTI 테스트</span>
+      </div>
+      <my-footer :selected="'home'" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MyHeader from "@/views/common/MyHeader.vue";
+import MyFooter from "@/views/common/MyFooter.vue";
+
+export default {
+  name: "Mbti",
+  components: {
+    MyHeader,
+    MyFooter,
+  },
+  // props
+  props: {},
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {},
+};
+</script>
+
+<style scoped src="./Mbti.css"></style>
