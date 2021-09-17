e215c0d5904ad6e6c7139d3104043ff6c957fc11
kimmy01 <nkim40181@hanmail.net>
2021-09-17 12:00:54 +0900

    chore: CorsOrigin추가
    Former-commit-id: 83db65e1146633abc88a6074a33caa7aa07b4cff

--- Backend/src/main/java/com/ssafy/buki/controller/UserController.java
+++ Backend/src/main/java/com/ssafy/buki/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
