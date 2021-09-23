1d85b564eb3d1d5931a502f2a7f60bc0af73561b
Wonsun Yoo <heidencena1@gmail.com>
2021-09-23 14:34:06 +0900

    [#S05P21A303-141] feat: 설문 기반 취미 카테고리 추천
    Former-commit-id: 190fda90aa50169ff089d5d033e4959783a6fea0

--- bigdata/app.py
+++ bigdata/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from flask_restx import Resource, Api
 
 from apis.recommendation import recommendation
+from apis.surveys import surveys
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +13,7 @@ def create_app():
     db = SQLAlchemy(app, session_options={'autocommit': True})
 
     cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    app.register_blueprint(surveys, url_prefix="/data/api/surveys")
     app.register_blueprint(recommendation, url_prefix="/data/api/recommend")
     
     return app
