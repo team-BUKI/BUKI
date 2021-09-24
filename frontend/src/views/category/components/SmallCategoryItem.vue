37cc18b455bbb6d4ea0f3beebca300d963ad35a4
원지연 <yh3347@naver.com>
2021-09-24 12:02:16 +0900

    [#S05P21A303-150] feat: 카테고리에 따른 소분류 카테고리 목록
    Former-commit-id: 541fc0c1a2ad216dd9680f8f26280f6b129cc175

---
+++ frontend/src/views/category/components/SmallCategoryItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <span class="text-button title-5" @click="clickButton">{{
+      item.name
+    }}</span>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "SmallCategoryItem",
+  components: {},
+  // props
+  props: {
+    item: Object,
+  },
+  // data
+  data() {
+    return {};
+  },
+  // computed
+  computed: {},
+  // lifecycle hook
+  mounted() {},
+  // methods
+  methods: {
+    ...mapActions("classStore", ["searchClassByCategory"]),
+    // 세부 카테고리로 클래스 검색
+    clickButton() {
+      // 카테고리 검색 결과 바꾸기
+      this.$router.replace({
+        path: this.$route.path,
+        query: {
+          bigcategory: this.$route.query.bigcategory,
+          smallcategory: this.item.id,
+        },
+      });
+      this.$router.go();
+    },
+  },
+};
+</script>
+
+<style scoped src="./SmallCategoryItem.css"></style>
