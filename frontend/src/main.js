e0f555d10de3804fc3c107378d3408cd9304ced5
원지연 <yh3347@naver.com>
2021-09-26 02:23:07 +0900

    [#S05P21A303-13] feat: vue-slider-component 플러그인 설치 및 CSS 수정
    Former-commit-id: 27b440d2dd515830e9ca818df2d3942c5b64eb25

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,22 +4,26 @@ import router from "./router";
 import store from "./store";
 import GAuth from "vue-google-oauth2";
 import InfiniteLoading from "vue-infinite-loading";
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/default.css";
 
-//google login
+// google login
 const gauthOption = {
   clientId:
     "805536219743-o3lrc58gsei4vh4e8fvb2c2rdf4l4c11.apps.googleusercontent.com",
   scope: "email profile",
   prompt: "select_account",
 };
-
 Vue.use(GAuth, gauthOption);
-Vue.use(InfiniteLoading, {
-  /* options */
-});
 
-//kakao login
+// kakao login
 window.Kakao.init("d90e243cba3532f958fc2750f411b28b");
+
+// vue-infinite-loading
+Vue.use(InfiniteLoading, {});
+
+// vue-slider-component
+Vue.component("VueSlider", VueSlider);
 
 Vue.config.productionTip = false;
 
