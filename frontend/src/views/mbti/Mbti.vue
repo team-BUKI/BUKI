5fbcd77038159db3eadde85f2a60142affd0f83f
원지연 <yh3347@naver.com>
2021-09-26 09:13:36 +0900

    [#S05P21A303-104] feat: MBTI 테스트 메인 화면
    Former-commit-id: 37429e85834e370a3366ef472af7f05da1228c5e

--- frontend/src/views/mbti/Mbti.vue
+++ frontend/src/views/mbti/Mbti.vue
@@ -1,9 +1,34 @@
 <template>
   <div>
     <div class="container">
-      <my-header />
-      <div class="contents">
+      <div class="header-wrapper">
+        <img
+          class="logo"
+          src="@/assets/images/logo.png"
+          @click="$router.push({ name: 'Home' })"
+        />
         <span class="title title-3">MBTI 테스트</span>
+      </div>
+      <div class="contents">
+        <img src="@/assets/images/mbti.png" alt="MBTI Test" class="banner" />
+        <div
+          class="button-4 title-5"
+          @click="$router.push({ name: 'MbtiTest' })"
+        >
+          시작하기
+        </div>
+        <span class="message title-6"
+          >테스트 결과를 저장 하시려면<br />로그인 후 테스트를
+          시작해주세요!</span
+        >
+        <div class="button-div">
+          <div class="kakao" @click="clickKakao">
+            <img src="@/assets/images/kakao.svg" class="icon" />
+          </div>
+          <div class="google" @click="clickGoogle">
+            <img src="@/assets/images/google.svg" class="icon" />
+          </div>
+        </div>
       </div>
       <my-footer :selected="'home'" />
     </div>
@@ -11,13 +36,11 @@
 </template>
 
 <script>
-import MyHeader from "@/views/common/MyHeader.vue";
 import MyFooter from "@/views/common/MyFooter.vue";
 
 export default {
   name: "Mbti",
   components: {
-    MyHeader,
     MyFooter,
   },
   // props
@@ -31,7 +54,10 @@ export default {
   // lifecycle hook
   mounted() {},
   // methods
-  methods: {},
+  methods: {
+    clickKakao() {},
+    clickGoogle() {},
+  },
 };
 </script>
 
