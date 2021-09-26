4ca45e22c183ddca56a70413a8251c6eb8c56c83
원지연 <yh3347@naver.com>
2021-09-26 09:06:04 +0900

    [#S05P21A303-144] fix: 클래스 모달창 기능 추가 및 코드 수정
    Former-commit-id: 016a7dd44cc8fd0b6f6d1a08640d51dc0d17e097

--- frontend/src/views/home/components/ClassModal.vue
+++ frontend/src/views/home/components/ClassModal.vue
@@ -3,6 +3,10 @@
     <div class="modal-container">
       <div class="class-info">
         <img :src="item.imageUrl" alt="Class Image" />
+        <div class="interest-div" @click="clickInterest">
+          <i v-if="item.interest" class="fas fa-heart interest"></i>
+          <i v-else class="far fa-heart no-interest"></i>
+        </div>
         <div class="class-sub-info">
           <div :class="'category-' + item.bigcategoryId">
             <span class="body-2">{{ smallcategoryName }}</span>
@@ -28,7 +32,7 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "ClassModal",
@@ -40,33 +44,56 @@ export default {
   },
   // data
   data() {
-    return {
-      smallcategoryName: "",
-      classAddress: "",
-      classPrice: "",
-    };
+    return {};
   },
   // computed
   computed: {
     ...mapState("classStore", ["smallcategory", "sido", "sigungu"]),
+    smallcategoryName: {
+      get() {
+        return this.smallcategory[this.item.smallcategoryId];
+      },
+      set() {},
+    },
+    classAddress: {
+      get() {
+        if (this.item.sidoId == 9) {
+          return this.sido[this.item.sidoId].name;
+        } else {
+          return this.sigungu[this.item.sigunguId];
+        }
+      },
+      set() {},
+    },
+    classPrice: {
+      get() {
+        let price = this.item.price.toLocaleString("ko-KR") + "원";
+        if (this.item.type == "일") {
+          return price;
+        } else if (this.item.type == "월") {
+          return "월 " + price;
+        } else {
+          return price + "/시간";
+        }
+      },
+      set() {},
+    },
   },
   // lifecycle hook
-  mounted() {
-    // 세부 카테고리 설정
-    this.smallcategoryName = this.smallcategory[this.item.smallcategoryId];
-    // 클래스 주소 설정
-    this.classAddress = this.sido[this.item.sidoId].name;
-    if (this.item.sidoId != 9) {
-      this.classAddress += " " + this.sigungu[this.item.sigunguId];
-    }
-    // 클래스 가격 설정
-    this.classPrice = this.item.price.toLocaleString("ko-KR") + "원";
-  },
+  mounted() {},
   // methods
   methods: {
+    ...mapActions("classStore", ["setInterestClass"]),
     // 클래스 정보 모달 닫기
     clickClose() {
       this.$emit("closeModal");
+    },
+    // 관심 클래스 등록여부 변경
+    clickInterest(event) {
+      event.stopPropagation();
+      let data = { hobbyClassId: this.item.id, interest: this.item.interest };
+      this.setInterestClass(data);
+      this.item.interest = !this.item.interest;
     },
     // 해당 클래스 사이트로 이동
     clickSiteButton() {
