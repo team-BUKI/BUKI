27e95fbffd7f7b58a34247d1a46f8141b80dd15c
Wonsun Yoo <heidencena1@gmail.com>
2021-09-29 00:38:04 +0900

    [#S05P21A303-157] chore: serverUri 변경
    Former-commit-id: 5a18d9a68d552067c4c4ac813ff1f5f5fd5282bf

--- frontend/src/constant/prod.js
+++ frontend/src/constant/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiServerUri: "https://j5a303.p.ssafy.io:9000",
+    apiServerUri: "https://j5a303.p.ssafy.io",
     apiFlaskServerUri: "https://j5a303.p.ssafy.io:5000",
     apiClientUri: "https://j5a303.p.ssafy.io",
 };
