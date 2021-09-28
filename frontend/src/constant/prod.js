b26614e35c326323ad0c0f76965ee9c3d6563a46
Wonsun Yoo <heidencena1@gmail.com>
2021-09-28 20:42:16 +0900

    [#S05P21A303-157] chore: 백엔드 포트번호 변경
    Former-commit-id: ea470368efc85de64f11f2db64a0219178684f47

--- frontend/src/constant/prod.js
+++ frontend/src/constant/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiServerUri: "https://j5a303.p.ssafy.io:8080",
+    apiServerUri: "https://j5a303.p.ssafy.io:9000",
     apiFlaskServerUri: "https://j5a303.p.ssafy.io:5000",
     apiClientUri: "https://j5a303.p.ssafy.io",
 };
